86dcf8a23666b1d8f2f5f0f3f84da2bdea778157
Will Leyton <wleylop22@gmail.com>
2021-12-23 12:12:49 -0500

    Add squants

---
+++ core/kernel/src/main/scala/com/leysoft/core/kernel/squants/data.scala
@@ -0,0 +1,12 @@
+package com.leysoft.core.kernel.squants
+
+import squants.market.Currency
+
+object data:
+   object COP
+       extends Currency(
+         "COP",
+         "CO Peso",
+         "COP $",
+         2
+       )
