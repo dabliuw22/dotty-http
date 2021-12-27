7ba56e295d96cac63bf841b6cbcc3d4fcfbc60a6
Will Leyton <wleylop22@gmail.com>
2021-12-27 08:52:47 -0500

    Add contextual

---
+++ core/kernel/src/main/scala/com/leysoft/core/kernel/context/contextual.scala
@@ -0,0 +1,44 @@
+package com.leysoft.core.kernel.context
+
+import cats.Applicative
+import cats.effect.Sync
+import cats.syntax.applicative.*
+import com.leysoft.core.kernel.context.data.Context
+import fs2.Stream
+
+object contextual:
+   /*
+   type Contextual = [F] =>> Context ?=> F
+    */
+   type Contextual[F] = Context ?=> F
+   object Contextual:
+      inline def apply[F[_]](using
+        F: Applicative[F]
+      ): Contextual[F[Context]] =
+        summon[Context].pure[F]
+
+   type ContextualStream = [F[_], O] =>> Context ?=> Stream[F, O]
+   object ContextualStream:
+      inline def apply[F[_]: Applicative]
+        : ContextualStream[F, Context] =
+        Stream.eval(summon[Context].pure[F])
+
+   trait ContextHandler[F[_]]:
+      def handle(ctx: Context): Contextual[F[Context]]
+      def get: Contextual[F[Context]]
+      def getS: ContextualStream[F, Context]
+
+   object ContextHandler:
+      inline def apply[F[_]](using
+        F: ContextHandler[F]
+      ): ContextHandler[F] = F
+
+      given [F[_]: Applicative: Sync]: ContextHandler[F] with
+         override def handle(using
+           ctx: Context
+         ): Contextual[F[Context]] =
+           Contextual[F]
+         override def get: Contextual[F[Context]]        =
+           summon[Context].pure[F]
+         override def getS: ContextualStream[F, Context] =
+           ContextualStream[F]
