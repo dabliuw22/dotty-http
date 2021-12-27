bd99e57c6e92eb1148a1d1b9e3135930c20e914f
Will Leyton <wleylop22@gmail.com>
2021-12-27 08:52:31 -0500

    Add data

---
+++ core/kernel/src/main/scala/com/leysoft/core/kernel/context/data.scala
@@ -0,0 +1,61 @@
+package com.leysoft.core.kernel.context
+
+import cats.{Eq, Order, Show}
+import cats.derived.semiauto.*
+import cats.syntax.show.*
+import com.leysoft.core.kernel.newtype.instances.given
+import io.circe.{Decoder, Encoder}
+import io.circe.syntax.*
+import io.circe.Codec
+import java.time.ZonedDateTime
+import java.util.UUID
+import fs2.Stream
+
+object data:
+   /*
+   import cats.derived.semiauto.*
+   import com.leysoft.core.kernel.newtype.data.*
+   type ContextId = ContextId.Type
+   object ContextId extends Newtype[String]:
+      inline def make: ContextId = ContextId(UUID.randomUUID.toString)
+
+   type ContextCreatedAt = ContextCreatedAt.Type
+   object ContextCreatedAt extends Newtype[ZonedDateTime]:
+      inline def make: ContextCreatedAt = ContextCreatedAt(
+        ZonedDateTime.now
+      )
+    */
+
+   case class ContextId(value: String)
+       derives Eq,
+         Show,
+         Order,
+         Codec.AsObject
+   object ContextId:
+      inline def make: ContextId = ContextId(UUID.randomUUID.toString)
+
+   case class ContextCreatedAt(value: ZonedDateTime)
+       derives Eq,
+         Show,
+         Order,
+         Codec.AsObject
+   object ContextCreatedAt:
+      inline def make: ContextCreatedAt = ContextCreatedAt(
+        ZonedDateTime.now
+      )
+
+   case class Context(
+     id: ContextId,
+     createdAt: ContextCreatedAt
+   ) derives Eq,
+         Show,
+         Order,
+         Codec.AsObject
+
+   object Context:
+      inline def make: Context =
+        Context(ContextId.make, ContextCreatedAt.make)
+
+      inline def from(id: ContextId): Context =
+        Context(id, ContextCreatedAt.make)
+end data
