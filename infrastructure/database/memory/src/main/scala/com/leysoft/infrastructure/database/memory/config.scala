2161a9b317e432521b0735cf2fa42065e708ef2b
Will Leyton <wleylop22@gmail.com>
2021-12-30 08:21:16 -0500

    Add redis config

---
+++ infrastructure/database/memory/src/main/scala/com/leysoft/infrastructure/database/memory/config.scala
@@ -0,0 +1,48 @@
+package com.leysoft.infrastructure.database.memory
+
+import cats.effect.Async
+import cats.effect.Resource
+import cats.syntax.functor.*
+import ciris.*
+import ciris.refined.*
+import com.leysoft.core.kernel.env.FromEnv
+import dev.profunktor.redis4cats.*
+import dev.profunktor.redis4cats.connection.*
+import dev.profunktor.redis4cats.RedisCommands
+import dev.profunktor.redis4cats.connection.{RedisClient, RedisURI}
+import dev.profunktor.redis4cats.data.RedisCodec
+import dev.profunktor.redis4cats.effect.Log.NoOp.*
+import dev.profunktor.redis4cats.Redis as InfraRedis
+import eu.timepit.refined.types.string.NonEmptyString
+
+object config:
+   enum Codec:
+      case Utf8
+
+   case class RedisConfiguration(
+     server: NonEmptyString,
+     codec: Codec = Codec.Utf8
+   )
+
+   object RedisConfiguration:
+      given [F[_]: Async]: FromEnv[F, RedisConfiguration] with
+         override def load: Resource[F, RedisConfiguration] =
+           env("REDIS_SERVER")
+             .as[NonEmptyString]
+             .map(server => RedisConfiguration(server))
+             .resource
+
+   extension (config: RedisConfiguration)
+      def stringCodec: RedisCodec[String, String] =
+        config.codec match
+           case Codec.Utf8 => RedisCodec.Utf8
+
+      def session[F[_]: Async]
+        : Resource[F, RedisCommands[F, String, String]] =
+        for {
+          uriRedis <-
+            Resource.eval(RedisURI.make[F](config.server.toString))
+          client   <- RedisClient[F].fromUri(uriRedis)
+          codec = config.stringCodec
+          cmd <- InfraRedis[F].fromClient(client, codec)
+        } yield cmd
