48f357943ba3dcc1b9f00bf77d83bab550e920fc
Will Leyton <wleylop22@gmail.com>
2022-01-11 16:37:29 -0500

    Update server

--- infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/HttpServer.scala
+++ infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/HttpServer.scala
@@ -3,40 +3,16 @@ package com.leysoft.infrastructure.http.server
 import cats.effect.{Async, ExitCode, Resource}
 import cats.syntax.functor.*
 import cats.syntax.semigroupk.*
-import com.leysoft.infrastructure.http.server.middleware.ContextMiddleware
 import config.HttpServerConfiguration
 import org.http4s.HttpApp
 import org.http4s.blaze.server.*
 import org.http4s.implicits.*
-import org.http4s.server.{Router, Server}
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
+import org.http4s.server.{Router, Server}
 import scala.concurrent.ExecutionContext
 
 object HttpServer:
-   inline def make[F[_]](using
-     F: Async[F]
-   ): ExecutionContext => HttpServerConfiguration => HttpApp[F] => F[
-     ExitCode
-   ] =
-     ctx =>
-       config =>
-         app =>
-           BlazeServerBuilder[F]
-             .withExecutionContext(ctx)
-             .bindHttp(config.port.value, config.host.value)
-             .withHttpApp(
-               CORS
-                 .policy
-                 .withAllowOriginAll
-                 .withAllowCredentials(false)
-                 .apply(ContextMiddleware(app))
-             )
-             .serve
-             .compile
-             .drain
-             .as(ExitCode.Success)
-
    inline def apply[F[_]](using
      F: Async[F]
    ): ExecutionContext => HttpServerConfiguration => HttpApp[
