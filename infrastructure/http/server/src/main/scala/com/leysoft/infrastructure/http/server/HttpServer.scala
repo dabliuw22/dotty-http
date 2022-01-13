a07068a94803119f1dadfaeac7f544d726dd1b24
Will Leyton <wleylop22@gmail.com>
2022-01-13 07:56:12 -0500

    Update

--- infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/HttpServer.scala
+++ infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/HttpServer.scala
@@ -3,13 +3,14 @@ package com.leysoft.infrastructure.http.server
 import cats.effect.{Async, ExitCode, Resource}
 import cats.syntax.functor.*
 import cats.syntax.semigroupk.*
+import com.leysoft.infrastructure.http.server.middleware.ContextMiddleware
 import config.HttpServerConfiguration
 import org.http4s.HttpApp
 import org.http4s.blaze.server.*
 import org.http4s.implicits.*
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
-import org.http4s.server.{Router, Server}
+import org.http4s.server.Server
 import scala.concurrent.ExecutionContext
 
 object HttpServer:
@@ -27,6 +28,7 @@ object HttpServer:
            BlazeServerBuilder[F]
              .withExecutionContext(ctx)
              .bindHttp(config.port.value, config.host.value)
+             .withBanner(Seq.empty)
              .withHttpApp(
                CORS
                  .policy
