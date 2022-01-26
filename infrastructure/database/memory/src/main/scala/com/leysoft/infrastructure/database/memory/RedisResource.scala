a6fa6bcdd9c7fa82d5b7908d854ce86aa796682d
Will Leyton <wleylop22@gmail.com>
2022-01-26 10:20:10 -0500

    Add RedisResource

---
+++ infrastructure/database/memory/src/main/scala/com/leysoft/infrastructure/database/memory/RedisResource.scala
@@ -0,0 +1,19 @@
+package com.leysoft.infrastructure.database.memory
+
+import cats.effect.{Async, Resource, Spawn}
+import com.leysoft.core.kernel.env.FromEnv
+import com.leysoft.infrastructure.database.memory.config.RedisConfiguration
+import com.leysoft.infrastructure.database.memory.instances.given
+import dev.profunktor.redis4cats.RedisCommands
+import org.typelevel.log4cats.Logger
+
+object RedisResource:
+   inline def apply[F[_]: Async](using
+     C: FromEnv[F, RedisConfiguration],
+     L: Logger[F]
+   ): Resource[F, Redis[F]] =
+     for
+        config                                            <- C.load
+        commands @ given RedisCommands[F, String, String] <-
+          config.session[F]
+     yield Redis[F]
