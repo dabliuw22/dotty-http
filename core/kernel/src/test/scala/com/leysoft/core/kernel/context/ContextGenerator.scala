eb6a25bd143d0657ab65fd00019dced99c7dbd63
Will Leyton <wleylop22@gmail.com>
2022-01-31 07:09:19 -0500

    Update context generator

--- core/kernel/src/test/scala/com/leysoft/core/kernel/context/ContextGenerator.scala
+++ core/kernel/src/test/scala/com/leysoft/core/kernel/context/ContextGenerator.scala
@@ -6,13 +6,9 @@ import org.scalacheck.Gen
 import java.time.ZonedDateTime
 
 object ContextGenerator:
-   val contextIdGen: Gen[ContextId]               =
+   val contextIdGen: Gen[ContextId] =
      Gen.uuid.map(_.toString).map(ContextId(_))
-   val contextCreatedAtGen: Gen[ContextCreatedAt] =
-     Gen.const(ZonedDateTime.now).map(ContextCreatedAt(_))
-   def gen: Gen[Context]                          =
-     for
-        id        <- contextIdGen
-        createdAt <- contextCreatedAtGen
-     yield Context(id, createdAt)
-   def simple: Context                            = Context.make
+   def gen: Gen[Context]            =
+     for id <- contextIdGen
+     yield Context(id)
+   def simple: Context              = Context.make
