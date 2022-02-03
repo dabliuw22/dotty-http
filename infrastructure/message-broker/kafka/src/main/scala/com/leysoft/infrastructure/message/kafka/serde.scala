ff33d9633f0fc78235ab634ea85d506b311804e9
Will Leyton <wleylop22@gmail.com>
2022-02-03 12:54:00 -0500

    Update serde

--- infrastructure/message-broker/kafka/src/main/scala/com/leysoft/infrastructure/message/kafka/serde.scala
+++ infrastructure/message-broker/kafka/src/main/scala/com/leysoft/infrastructure/message/kafka/serde.scala
@@ -1,35 +1,54 @@
 package com.leysoft.infrastructure.message.kafka
 
 import cats.effect.Async
+import cats.syntax.applicative.*
+import cats.syntax.applicativeError.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.databind.{DeserializationFeature, SerializationFeature}
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.leysoft.core.kernel.message.data.Message
+import com.leysoft.core.kernel.error.data.BusinessError
+import com.leysoft.core.kernel.message.data.*
 import com.sun.net.httpserver.Authenticator.Success
-import fs2.kafka.Deserializer as KafkaDeserializer
-import fs2.kafka.Serializer as KafkaSerializer
-import java.util.Map as JavaMap
+import fs2.kafka.Deserializer
+import fs2.kafka.Serializer
 import org.apache.kafka.common.serialization.*
+
+import java.util.TimeZone
 import scala.util.Try
 
 object serde:
-   object Json:
+   private object Json:
       private val Mapper = JsonMapper
         .builder
         .addModule(DefaultScalaModule)
         .addModule(new Jdk8Module)
         .addModule(new JavaTimeModule)
         .configure(
+          SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+          false
+        )
+        .configure(
+          SerializationFeature.WRITE_DATES_WITH_CONTEXT_TIME_ZONE,
+          false
+        )
+        .configure(
           DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
           false
         )
         .configure(
-          SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+          DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
           false
         )
+        .configure(
+          DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY,
+          true
+        )
         .build
+        .setTimeZone(TimeZone.getDefault)
 
       def write[A <: Message](data: A): Option[String] = Try(
         Mapper.writeValueAsString(data)
@@ -38,33 +57,49 @@ object serde:
         Mapper.readValue(data, classOf[Message]).asInstanceOf[A]
       ).toOption
 
-   private class JsonSerde[A <: Message]
-       extends Serializer[A]
-       with Deserializer[A]
-       with Serde[A]:
-      private val ser            = new StringSerializer
-      private val des            = new StringDeserializer
-      override def configure(
-        configs: JavaMap[String, ?],
-        isKey: Boolean
-      ): Unit = super.configure(configs, isKey)
-      override def close(): Unit = super.close()
-      override def serialize(topic: String, data: A): Array[Byte]   =
-        Json.write(data) match
-           case Some(json) => ser.serialize(topic, json)
-           case _ => throw new RuntimeException("Write Error")
-      override def deserialize(topic: String, data: Array[Byte]): A =
-        Json.read(des.deserialize(topic, data)) match
-           case Some(value) => value
-           case _ => throw new RuntimeException("Read Error")
-      override def serializer(): Serializer[A]     = this
-      override def deserializer(): Deserializer[A] = this
+   case class SerializerError(message: String, code: String)
+       extends BusinessError(message, code)
+   object SerializerError:
+      inline def apply(message: String): BusinessError =
+        SerializerError(message, "00040")
+   case class DeserializerError(message: String, code: String)
+       extends BusinessError(message, code)
+   object DeserializerError:
+      inline def apply(message: String): BusinessError =
+        DeserializerError(message, "00040")
 
-   def keyDeserializer[F[_]: Async]: KafkaDeserializer[F, String]    =
-     KafkaDeserializer[F, String]
-   def valueDeserializer[F[_]: Async]: KafkaDeserializer[F, Message] =
-     KafkaDeserializer.delegate[F, Message](JsonSerde())
-   def keySerializer[F[_]: Async]: KafkaSerializer[F, String]        =
-     KafkaSerializer[F, String]
-   def valueSerializer[F[_]: Async]: KafkaSerializer[F, Message]     =
-     KafkaSerializer.delegate[F, Message](JsonSerde())
+   given [F[_]: Async](using
+     D: Deserializer[F, String]
+   ): Deserializer[F, Message] =
+     Deserializer.instance { (topic, header, data) =>
+       D
+         .deserialize(topic, header, data)
+         .map(Json.read[Message])
+         .flatMap(
+           _.fold(
+             Async[F]
+               .raiseError(
+                 DeserializerError("Could not deserialize record")
+               )
+           )(_.pure[F])
+         )
+     }
+
+   given [F[_]: Async](using
+     S: Serializer[F, String]
+   ): Serializer[F, Message] =
+     Serializer.instance[F, Message] { (topic, header, data) =>
+       Json
+         .write[Message](data)
+         .pure[F]
+         .flatMap(
+           _.fold(
+             Async[F].raiseError(
+               SerializerError("Could not serialize record")
+             )
+           )(_.pure[F])
+         )
+         .flatMap(
+           S.serialize(topic, header, _)
+         )
+     }
