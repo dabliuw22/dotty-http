9d8c885e6e2890df9f5ab937891499ccb68f09cb
Will Leyton <wleylop22@gmail.com>
2022-03-02 13:29:43 -0500

    ÂUpdate Producer

--- core/kernel/src/main/scala/com/leysoft/core/kernel/message/Producer.scala
+++ core/kernel/src/main/scala/com/leysoft/core/kernel/message/Producer.scala
@@ -7,7 +7,7 @@ import com.leysoft.core.kernel.message.data.*
 trait Producer[F[_]]:
    def execute[A <: Message](
      message: A
-   ): Contextual[F[MessageMetadata]]
+   ): Kind[F, MessageMetadata]
 
 object Producer:
    inline def apply[F[_]](using F: Producer[F]): Producer[F] = F
