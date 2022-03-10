d073820ca860889b265f5ae7e4607d10dcf3a1b4
Will Leyton <wleylop22@gmail.com>
2022-03-10 10:03:14 -0500

    Update context middleware

--- infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/middleware/context.scala
+++ infrastructure/http/server/src/main/scala/com/leysoft/infrastructure/http/server/middleware/context.scala
@@ -3,14 +3,14 @@ package com.leysoft.infrastructure.http.server.middleware
 import cats.effect.Async
 import cats.syntax.flatMap.*
 import cats.syntax.functor.*
-import com.leysoft.core.kernel.context.contextual.Contextual
+import com.leysoft.core.kernel.context.contextual.*
 import com.leysoft.core.kernel.context.data.*
 import com.leysoft.infrastructure.http.kernel.headers.*
 import org.http4s.{Request, Response}
 
-object ContextMiddleware:
+object context:
    extension [F[_]: Async](request: Request[F])
-     def handle(f: Contextual[F[Response[F]]]): F[Response[F]] =
+     def handle(f: Kind[F, Response[F]]): F[Response[F]] =
        for
           context @ given Context <- request.getContext
           response                <- f
