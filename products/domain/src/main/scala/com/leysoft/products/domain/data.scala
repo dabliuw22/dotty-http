137d8192ec740a4ffacef5c7a140f5fa97e999e9
Will Leyton <wleylop22@gmail.com>
2022-06-07 08:07:21 -0500

    Update

--- products/domain/src/main/scala/com/leysoft/products/domain/data.scala
+++ products/domain/src/main/scala/com/leysoft/products/domain/data.scala
@@ -1,17 +1,29 @@
 package com.leysoft.products.domain
 
+import com.leysoft.core.kernel.domain.product.data.ProductId as KernelProductId
+import com.leysoft.core.kernel.domain.product.data.ProductName as KernelProductName
+import com.leysoft.core.kernel.domain.product.data.ProductStock as KernelProductStock
+import com.leysoft.core.kernel.domain.product.data.ProductCreatedAt as KernelProductCreatedAt
+import com.leysoft.core.kernel.domain.product.data.Product as KernelProduct
+
 import java.time.ZonedDateTime
 import java.util.UUID
 
 object data:
-   case class ProductId(value: String)    extends AnyVal
+   case class ProductId(value: String)    extends AnyVal:
+      def toKernel: KernelProductId = KernelProductId(value)
    object ProductId:
       inline def make: ProductId = ProductId(UUID.randomUUID.toString)
-   case class ProductName(value: String)  extends AnyVal
-   case class ProductStock(value: Double) extends AnyVal
+   case class ProductName(value: String)  extends AnyVal:
+      def toKernel: KernelProductName = KernelProductName(value)
+   case class ProductStock(value: Double) extends AnyVal:
+      def toKernel: KernelProductStock = KernelProductStock(value)
    case class ProductCreatedAt(
      value: ZonedDateTime
-   ) extends AnyVal
+   ) extends AnyVal:
+      def toKernel: KernelProductCreatedAt = KernelProductCreatedAt(
+        value
+      )
    object ProductCreatedAt:
       inline def make: ProductCreatedAt = ProductCreatedAt(
         ZonedDateTime.now
@@ -21,10 +33,25 @@ object data:
      name: ProductName,
      stock: ProductStock,
      createdAt: ProductCreatedAt
-   )
+   ):
+      def toKernel: KernelProduct = KernelProduct(
+        id = id.toKernel,
+        name = name.toKernel,
+        stock = stock.toKernel,
+        createdAt = createdAt.toKernel
+      )
    object Product:
       inline def make(
         name: ProductName,
         stock: ProductStock
       ): Product =
         Product(ProductId.make, name, stock, ProductCreatedAt.make)
+
+   extension (product: KernelProduct)
+     def to: Product =
+       Product(
+         ProductId(product.id.value),
+         ProductName(product.name.value),
+         ProductStock(product.stock.value),
+         ProductCreatedAt(product.createdAt.value)
+       )
