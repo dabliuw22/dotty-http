83af90e457081037bea4821780414f846db4c423
Will Leyton <wleylop22@gmail.com>
2022-09-22 11:46:29 -0500

    Add api config

---
+++ api/src/main/scala/com/leysoft/api/config.scala
@@ -0,0 +1,18 @@
+package com.leysoft.api
+
+import cats.effect.{Async, Resource}
+import ciris.*
+import ciris.refined.*
+import com.leysoft.core.kernel.env.FromEnv
+import eu.timepit.refined.types.string.NonEmptyString
+
+object config:
+   case class ApiConfiguration(clientId: NonEmptyString)
+
+   object ApiConfiguration:
+      given [F[_]: Async]: FromEnv[F, ApiConfiguration] with
+         override def load: Resource[F, ApiConfiguration] =
+           env("API_CLIENT_ID")
+             .as[NonEmptyString]
+             .map(ApiConfiguration(_))
+             .resource
