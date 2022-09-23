c63f320778663849150f763b57d08eb9e5d41d28
Will Leyton <wleylop22@gmail.com>
2022-09-23 09:39:39 -0500

    Add test commands

---
+++ commands/src/main/scala/com/leysoft/commands/TestCommandTwo.scala
@@ -0,0 +1,28 @@
+package com.leysoft.commands
+
+import com.leysoft.core.kernel.context.data.Context
+import com.leysoft.core.kernel.message.data.*
+
+import java.util.UUID
+
+case class TestCommandTwo(
+  override val id: MessageId,
+  override val metadata: MessageMetadata,
+  override val context: Context,
+  name: String
+) extends Command
+
+object TestCommandTwo:
+   def make(using
+     ctx: Context
+   )(name: String): Command =
+     TestCommandTwo(
+       MessageId(),
+       MessageMetadata(
+         MessageChannel("commands.channel"),
+         MessageKey(UUID.randomUUID.toString),
+         MessageCreatedAt()
+       ),
+       ctx,
+       name
+     )
